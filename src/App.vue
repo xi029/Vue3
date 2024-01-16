0ca522f341163fba8310cb3589f830d4fe5a8c27
tai xian <fengyuan711@gmail.com>
2024-01-15 16:32:52 -0800

    Your commit message

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--html-->
+  <div class="app">
+    <h1>你好啊！</h1>
+    <person />
+  </div>
+</template>
+
+<script lang="ts">
+import Person from "./components/Person.vue";
+// Js或TS
+export default {
+  name: "App", //组件名
+  components: { Person }, //注册组件
+};
+</script>
+
+<style>
+/*样式*/
+.app {
+  background-color: aqua;
+  box-shadow: 0 0 10px;
+  border-radius: 20px;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "张三",
+      age: 18,
+      tel: "138888888888",
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "zhang-san";
+    },
+    changeAge() {
+      this.age += 1;
+    },
+    showTel() {
+      alert(this.tel);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.person {
+  background-color: slateblue;
+  box-shadow: 0 0 10px;
+  border-radius: 20px;
+  padding: 20px;
+}
+button {
+  margin: 0 5px;
+}
+</style>
